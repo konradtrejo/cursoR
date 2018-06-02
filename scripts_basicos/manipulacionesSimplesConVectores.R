d79b7a6096b21799ec84704ab7f0ecfc7d37a13d
konrad <kbtch95@gmail.com>
2018-06-01 20:58:25 -0500

    manipulacion de vectores  version 2.0

---
+++ scripts_basicos/manipulacionesSimplesConVectores.R
@@ -0,0 +1,26 @@
+#vector numerico: es una coleccion ordenada de numeros
+
+x <- c(7,3,4,3,8.9) #asiganacion de x
+x
+
+#asignacion con un vectro antes definido 
+Y <-c(73,x,0)
+Y
+
+#operacion de un numero por un vector
+#suma
+z <-Y +3
+z
+
+#multipicacion 
+z1<- Y*4
+z1
+
+#sumar vectores de igual lengh
+w1 <- c(1,45,17,47,8,10) + c(4,18,75,100,3,4)
+w1
+
+#vector de caracteres  :(
+x1 <-c("holaaaaaa","adiossssss","XD","._. ")
+x1
+
